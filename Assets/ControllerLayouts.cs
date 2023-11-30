86cd1aa5ca94fe447c77f310b7ead2d225746a79
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-11-30 11:30:29 +0200

    untested dualsense attempt

---
+++ Assets/ControllerLayouts.cs
@@ -0,0 +1,34 @@
+public class ControllerLayouts
+{
+    public const string LayoutDS4 = @"{
+      ""name"": ""DualShock4GamepadHIDCustom"",
+      ""extend"": ""DualShock4GamepadHID"",
+      ""controls"": [
+        {""name"":""gyro"", ""format"":""VC3S"", ""offset"":13,
+         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
+        {""name"":""gyro/x"", ""format"":""SHRT"", ""offset"":0 },
+        {""name"":""gyro/y"", ""format"":""SHRT"", ""offset"":2 },
+        {""name"":""gyro/z"", ""format"":""SHRT"", ""offset"":4 },
+        {""name"":""accel"", ""format"":""VC3S"", ""offset"":19,
+         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
+        {""name"":""accel/x"", ""format"":""SHRT"", ""offset"":0 },
+        {""name"":""accel/y"", ""format"":""SHRT"", ""offset"":2 },
+        {""name"":""accel/z"", ""format"":""SHRT"", ""offset"":4 }
+      ]}";
+
+    public const string LayoutDS5 = @"{
+      ""name"": ""DualSenseGamepadHIDCustom"",
+      ""extend"": ""DualSenseGamepadHID"",
+      ""controls"": [
+        {""name"":""gyro"", ""format"":""VC3S"", ""offset"":16,
+         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
+        {""name"":""gyro/x"", ""format"":""SHRT"", ""offset"":0 },
+        {""name"":""gyro/y"", ""format"":""SHRT"", ""offset"":2 },
+        {""name"":""gyro/z"", ""format"":""SHRT"", ""offset"":4 },
+        {""name"":""accel"", ""format"":""VC3S"", ""offset"":22,
+         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
+        {""name"":""accel/x"", ""format"":""SHRT"", ""offset"":0 },
+        {""name"":""accel/y"", ""format"":""SHRT"", ""offset"":2 },
+        {""name"":""accel/z"", ""format"":""SHRT"", ""offset"":4 }
+      ]}";
+}
--- Assets/GyroInputTester.cs
+++ Assets/GyroInputTester.cs
@@ -1,29 +1,11 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
-using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.DualShock;
 
 sealed class GyroInputTester : MonoBehaviour
 {
     #region Static class members
-
-    // Layout extension written in JSON
-    const string LayoutJson = @"{
-      ""name"": ""DualShock4GamepadHIDCustom"",
-      ""extend"": ""DualShock4GamepadHID"",
-      ""controls"": [
-        {""name"":""gyro"", ""format"":""VC3S"", ""offset"":13,
-         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
-        {""name"":""gyro/x"", ""format"":""SHRT"", ""offset"":0 },
-        {""name"":""gyro/y"", ""format"":""SHRT"", ""offset"":2 },
-        {""name"":""gyro/z"", ""format"":""SHRT"", ""offset"":4 },
-        {""name"":""accel"", ""format"":""VC3S"", ""offset"":19,
-         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
-        {""name"":""accel/x"", ""format"":""SHRT"", ""offset"":0 },
-        {""name"":""accel/y"", ""format"":""SHRT"", ""offset"":2 },
-        {""name"":""accel/z"", ""format"":""SHRT"", ""offset"":4 }
-      ]}";
-
     // Gyro vector data to rotation conversion
     static Quaternion GyroInputToRotation(in InputAction.CallbackContext ctx)
     {
@@ -55,8 +37,21 @@ sealed class GyroInputTester : MonoBehaviour
 
     void Start()
     {
-        // DS4 input layout extension
-        InputSystem.RegisterLayoutOverride(LayoutJson);
+        // input layout extension
+        switch (Gamepad.current)
+        {
+            case DualSenseGamepadHID:
+                Debug.Log("DualSense detected.");
+                InputSystem.RegisterLayoutOverride(ControllerLayouts.LayoutDS5);
+                break;
+            case DualShock4GamepadHID:
+                Debug.Log("DualShock 4 detected.");
+                InputSystem.RegisterLayoutOverride(ControllerLayouts.LayoutDS4);
+                break;
+            default:
+                Debug.Log("Unsupported controller.");
+                return;
+        }
 
         // Gyroscope input callback
         var action = new InputAction(binding: "<Gamepad>/gyro");
